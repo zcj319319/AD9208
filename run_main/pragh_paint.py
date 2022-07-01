d42b8177e4d809c37047f6727fcb850a0c90ba29
zcj319319 <18817281502@163.com>
2022-07-01 18:56:59 +0800

    参数初始化

--- run_main/pragh_paint.py
+++ run_main/pragh_paint.py
@@ -18,7 +18,25 @@ class config_info:
 
 class param_info:
     def __init__(self):
-        pass
+        self.sigbw = 100e6
+        self.dpdbw = 300e6
+        self.sideband = 3000
+        self.sideband_sig = 10e6
+        self.fullscale = 1200
+        self.Rl = 100
+        self.num_interleave = 4
+        self.num_HD = 5
+        self.window = 'hann'
+        self.nyquitst_zone = 2
+        self.dacOSR = 1
+        self.imd_mode = 1
+        self.plot_range = 0
+        self.simple_plot = 0
+        self.dc_1f_noise_cancel = 20e6
+        self.dbc_th_HD = -20
+        self.dbc_th_IMD = -20
+        self.dbc_th_IL = -20
+        self.dbc_th_SFDR = -20
 
 def paint_widget(handle):
     pass
